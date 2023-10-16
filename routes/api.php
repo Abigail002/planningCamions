96bb0f67f20f6881698364498707ae07419f59ac
Abigail002 <abigailameogno98@gmail.com>
2023-10-16 16:08:37 +0000

    Implementation of login function

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::name('api.')->group(
+    function () {
+        Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+            return new UserResource($request->user());
+        });
+
+        Route::middleware('guest')->group(function () {
+            Route::post('/login', LoginController::class)->name('login');
+        });
+    }
+);
