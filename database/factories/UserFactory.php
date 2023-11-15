25044e50405875996d6bacbde080793792f4b27c
Abigail002 <abigailameogno98@gmail.com>
2023-11-15 17:17:00 +0000

    Intallation of spatie package to manage user permissions and roles

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'lastname' => fake()->lastname(),
+/*             'lastname' => fake()->lastname(),
             'firstname' => fake()->firstname(),
             'username' => fake()->username(),
             'contact' => fake()->phoneNumber('FR'),
@@ -26,7 +26,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-        ];
+ */        ];
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,15 +17,21 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        /*         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+/*         $user = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@medlog.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'contact' => '98788654',
         ]);
  */
-        $this->call([
+        /*         $this->call([
             TrailerSeeder::class,
             TruckSeeder::class,
             ContainerTypeSeeder::class,
         ]);
+ */
+        $role = Role::create(['name' => 'CoordinationOfficer']);
+        $role2 = Role::create(['name' => 'OperationOfficer']);
+        //$user->assignRole($role);
     }
 }
