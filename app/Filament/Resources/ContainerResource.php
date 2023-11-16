da70ab39e293453908929fe84093cc7999cef148
Abigail002 <abigailameogno98@gmail.com>
2023-11-16 13:26:36 +0000

    Add of ContainerResource

---
+++ app/Filament/Resources/ContainerResource.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContainerResource\Pages;
+use App\Filament\Resources\ContainerResource\RelationManagers;
+use App\Models\Container;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ContainerResource extends Resource
+{
+    protected static ?string $model = Container::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('container_type_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('number')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('weight')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('workOrder')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('container_type_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('number')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('weight')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('workOrder')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContainers::route('/'),
+            'create' => Pages\CreateContainer::route('/create'),
+            'edit' => Pages\EditContainer::route('/{record}/edit'),
+        ];
+    }
+}
